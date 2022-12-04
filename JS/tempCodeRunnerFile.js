2951be4e8635448d47604ace3581e913444c0d4f
Roman Greshchik <95360800+methfoxxkaiju@users.noreply.github.com>
2022-12-04 12:35:44 +0400

    Delete tempCodeRunnerFile.js

--- JS/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-let copySorted = (arr) => {
-    for (let i = 0; i < arr.lenght; i++) {
-        return arr[i].split("");
-    }
-}
-
-let arr = ["HTML", "JavaScript", "CSS"];
-alert(copySorted(arr))
